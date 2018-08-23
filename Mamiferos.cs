e1a0afae41b95c10d2e31eafcdacf39e8b268cc9
Eduardo159 <42323402+Eduardo159@users.noreply.github.com>
2018-08-22 21:08:52 -0500

    lo que llevaba

---
+++ Mamiferos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace zoologico
+{
+    class Mamiferos
+    {
+        private string vertebrados;
+        private string glandmamarias;
+        private string placenta;
+        private ushort patas;
+
+        public void Vertebrados()
+        {
+            vertebrados = "Todos los mamiferos son vertebrados";
+            
+            Console.WriteLine("Caracteristica uno de los mamiferos: \n\n{0}", vertebrados);
+        }
+        public void glandMamarias()
+        {
+            glandmamarias = "Todos los mamiferos tienen glandulas mamarias para alimentar a sus crias";
+            Console.WriteLine("Caracteristica dos: {0}", glandmamarias);
+        }
+        public void Placenta()
+        {
+            placenta = "Han desarrollado placenta, lo que les ha permitido sobrevivir en el medio terrestre";
+            Console.WriteLine("Caracteristica tres: {0}", placenta);
+        }
+    }
+}
