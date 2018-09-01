bc0c183cbc33b6257026be466f598589b9f50078
NormanBrandon <normanbrandons11@gmail.com>
2018-08-31 23:45:35 -0500

    Listo p4

--- Práctica 4/Práctica 4/Program.cs
+++ Práctica 4/Práctica 4/Program.cs
@@ -16,7 +16,21 @@ namespace Práctica_4
             Console.WriteLine("El promedio del alumno 2 es: " + promedio2);
             int promedio3 = miPromedio.Alumno1(7, 4, 8, 7, 2);
             Console.WriteLine("El promedio del alumno 3 es: " + promedio3);
+            Console.WriteLine("Presione Enter para continuar");
+            Console.ReadLine();
+            R3 v1 = new R3(1, 2, 3);
+            R3 v2 = new R3(4, 5, 6);
+            R3 v3 = v1 + v2;
+            Console.WriteLine("La suma de "+v1+" y "+ v2+" es :"+ v3);
+         
+            Console.WriteLine("La norma de " + v3 +" es :"+ !v3);
+            
+            Console.WriteLine("El producto interno de  " + v1 + " y " + v2 + " es :"+ v1*v2);
 
+
+
+            Console.WriteLine("Presione Enter para continuar");
+            Console.ReadLine();
         }
     }
 }
