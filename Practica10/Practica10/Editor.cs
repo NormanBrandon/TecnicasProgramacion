20c70386950d76ec9a43d28e9fc2595b7d983bf2
Eduardo159 <eduardomarquez.98@hotmail.com>
2018-09-28 02:37:13 -0500

    Inicio de practica

---
+++ Practica10/Practica10/Editor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practica10
+{
+    class Editor
+    {
+        public delegate string MiManejadorEvento();
+        public event MiManejadorEvento DisparaEvento1;
+
+        public delegate int MiManejadorEvento2();
+        public event MiManejadorEvento2 DisparaEvento2;
+
+
+        public string DisparaMetodo()
+        {
+            return DisparaEvento1();
+        }
+
+        public int DisparaEdad()
+        {
+            return DisparaEvento2();
+        }
+
+    }
+}
---
+++ Practica10/Practica10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Practica10
+{
+    class Program
+    {
+        private string cadena;
+        private int edad;
+
+        public void PideCadena()
+        {
+            Console.WriteLine("Ingresa tu nombre");
+            cadena = Console.ReadLine();
+        }
+
+        public string MandaCadena()
+        {
+            return cadena;
+        }
+
+        public void PideEdad()
+        {
+            Console.WriteLine("Ingresa tu edad");
+            edad =int.Parse( Console.ReadLine() );
+        }
+
+        public int Edad()
+        {
+            return edad;
+        }
+
+
+        static void Main()
+        {
+            Program miProgram = new Program();
+            Editor miEditor = new Editor();
+
+            miProgram.PideCadena();
+            miEditor.DisparaEvento1 += miProgram.MandaCadena;
+            string cadenaFinal = miEditor.DisparaMetodo();
+            Console.WriteLine("Tu nombre es: " + cadenaFinal);
+
+            miProgram.PideEdad();
+            miEditor.DisparaEvento2 += miProgram.Edad;
+            int edadFinal = miEditor.DisparaEdad();
+            Console.WriteLine("Tu edad es: " + edadFinal);
+
+        }
+    }
+}
