656abe8038c5d5aaa0ceee7297eb1662526439ea
NormanBrandon <normanbrandons11@gmail.com>
2018-09-28 23:42:33 -0500

    Practica 10

---
+++ P10/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Practica10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+        public delegate void Evento();
+        public event Evento DisparaEvent2;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAumentar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDisminuir = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tiempo = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAumentar
+            // 
+            this.btnAumentar.Location = new System.Drawing.Point(260, 228);
+            this.btnAumentar.Name = "btnAumentar";
+            this.btnAumentar.Size = new System.Drawing.Size(75, 55);
+            this.btnAumentar.TabIndex = 0;
+            this.btnAumentar.Text = "Aumentar Velocidad";
+            this.btnAumentar.UseVisualStyleBackColor = true;
+            this.btnAumentar.Click += new System.EventHandler(this.btnAumentar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(151, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 23);
+            this.label6.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(212, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 54);
+            this.label1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(151, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 23);
+            this.label7.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(212, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 54);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(151, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 23);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(130, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 54);
+            this.label4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(130, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 54);
+            this.label5.TabIndex = 2;
+            // 
+            // btnDisminuir
+            // 
+            this.btnDisminuir.Location = new System.Drawing.Point(27, 228);
+            this.btnDisminuir.Name = "btnDisminuir";
+            this.btnDisminuir.Size = new System.Drawing.Size(75, 55);
+            this.btnDisminuir.TabIndex = 0;
+            this.btnDisminuir.Text = "Disminuir Velocidad";
+            this.btnDisminuir.UseVisualStyleBackColor = true;
+            this.btnDisminuir.Click += new System.EventHandler(this.btnDisminuir_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(27, 153);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 55);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Resetear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(260, 153);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(75, 55);
+            this.btnParar.TabIndex = 3;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Ingrese un numero del 0 al 9";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(104, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(190, 18);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Display de 7 Segmentos";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(260, 81);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 55);
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tiempo
+            // 
+            this.tiempo.Location = new System.Drawing.Point(240, 50);
+            this.tiempo.Name = "tiempo";
+            this.tiempo.Size = new System.Drawing.Size(55, 23);
+            this.tiempo.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(301, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 288);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tiempo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDisminuir);
+            this.Controls.Add(this.btnAumentar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAumentar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDisminuir;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label tiempo;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ Practica10/Practica10/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Practica10
+{
+    public partial class Form1 : Form
+    {
+
+        short contador = 0;
+        int contado2 = 0;
+        ArrayList labels = new ArrayList();
+        Label lb;
+        public delegate void Manejador();
+        public delegate void Manejador2();
+        public delegate void Manejador3();
+        public event Manejador DisparaEvento1;
+        public event Manejador DisparaEvento2;
+        public event Manejador2 DisparaEvento3;
+        public event Manejador2 DisparaEvento4;
+        public event Manejador3 DisparaEvento5;
+        public event Manejador3 DisparaEvento6;
+
+
+
+        public bool[,] numeros = {
+        { true, true, true, true, true, true, false},
+        { true, true, false, false, false, false, false },
+        { true, false, true, true, false, true, true},
+        { true, true, true, false, false, true, true},
+        { true, true, false, false, true, false, true},
+        { false, true, true, false, true, true, true},
+        { false, true, true, true, true, true, true},
+        { true, true, false, false, false, true, false},
+        { true, true, true, true, true, true, true},
+        { true, true, true, false, true, true, true},
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Metodos de Eventos
+        private void validarContador() {
+            if (contador >= 9)
+            {
+                contador = 0;
+            }
+            else
+            {
+                contador++;
+            }
+
+        }
+        private void validarTmax()
+        {
+            if (timer1.Interval > 2000)
+            {
+                timer1.Interval = 1950;
+                MessageBox.Show("El limite de tiempo maximo es de 2 s", "Error");
+            }
+        }
+
+        private void validarTmin()
+        {
+            if (timer1.Interval < 100)
+            {
+                timer1.Interval = 100;
+                MessageBox.Show("El limite de tiempo minimo es de 100 ms", "Error");
+            }
+        }
+        private void versionPro()
+        {
+            contado2++;
+            if (contado2 % 5==0)
+            {
+                MessageBox.Show("Cambie a la version pro por solo $9.99 al mes", "Gracias por usar nuestra app");
+            }
+        }
+        #endregion
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DisparaEvento1();
+            setNumber();
+
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            setNumber();
+            DisparaEvento4();
+        }
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            DisparaEvento6();
+            tiempo.Text = "" + timer1.Interval;
+        }
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            DisparaEvento5();
+        }
+        private void btnDisminuir_Click(object sender, EventArgs e)
+        {
+            timer1.Interval += 50;
+            
+            DisparaEvento3();
+            tiempo.Text = "" + timer1.Interval;
+            DisparaEvento6();
+        }
+
+        private void btnAumentar_Click(object sender, EventArgs e)
+        {
+            timer1.Interval -= 50;            
+            DisparaEvento2();
+            tiempo.Text = "" + timer1.Interval;
+            DisparaEvento6();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == (int)Keys.Enter)
+            {
+
+                contador = short.Parse(textBox1.Text);
+            }
+            setNumber();
+        }
+  
+
+
+        private void setNumber() {
+            for (int j = 0; j < 7; j++)
+            {
+                lb = (Label)labels[j];
+                lb.Visible = numeros[contador, j];
+            }
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labels.Add(label1);
+            labels.Add(label2);
+            labels.Add(label3);
+            labels.Add(label4);
+            labels.Add(label5);
+            labels.Add(label6);
+            labels.Add(label7);
+            DisparaEvento1 += this.validarContador;
+            DisparaEvento2 += this.validarTmin;
+            DisparaEvento3 += this.validarTmax;
+            DisparaEvento4 += this.versionPro;
+            DisparaEvento5 += this.versionPro;
+            DisparaEvento6 += this.versionPro;
+
+        }
+
+
+    }
+}
