49ac9e1fc6b7998754ce3c97ee51efc8ab0e5dab
NormanBrandon <normanbrandons11@gmail.com>
2018-10-22 23:37:49 -0500

    Arboles2

--- Practica15/Arboles/Form1.Designer.cs
+++ Practica15/Arboles/Form1.Designer.cs
@@ -32,21 +32,28 @@
             this.ptb = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbaltura = new System.Windows.Forms.Label();
+            this.lbgrado = new System.Windows.Forms.Label();
+            this.lborden = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ptb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // ptb
             // 
-            this.ptb.Location = new System.Drawing.Point(29, 60);
+            this.ptb.Location = new System.Drawing.Point(3, 54);
             this.ptb.Name = "ptb";
-            this.ptb.Size = new System.Drawing.Size(742, 384);
+            this.ptb.Size = new System.Drawing.Size(698, 384);
             this.ptb.TabIndex = 0;
             this.ptb.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(289, 34);
+            this.textBox1.Location = new System.Drawing.Point(287, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(209, 20);
             this.textBox1.TabIndex = 1;
@@ -56,11 +63,89 @@
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(755, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(755, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Grado";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(755, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Orden";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbaltura
+            // 
+            this.lbaltura.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbaltura.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbaltura.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbaltura.Location = new System.Drawing.Point(814, 64);
+            this.lbaltura.Name = "lbaltura";
+            this.lbaltura.Size = new System.Drawing.Size(64, 23);
+            this.lbaltura.TabIndex = 2;
+            this.lbaltura.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbgrado
+            // 
+            this.lbgrado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbgrado.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbgrado.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbgrado.Location = new System.Drawing.Point(814, 175);
+            this.lbgrado.Name = "lbgrado";
+            this.lbgrado.Size = new System.Drawing.Size(64, 23);
+            this.lbgrado.TabIndex = 2;
+            this.lbgrado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lborden
+            // 
+            this.lborden.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lborden.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lborden.ForeColor = System.Drawing.SystemColors.Control;
+            this.lborden.Location = new System.Drawing.Point(814, 226);
+            this.lborden.Name = "lborden";
+            this.lborden.Size = new System.Drawing.Size(64, 23);
+            this.lborden.TabIndex = 2;
+            this.lborden.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 59);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "I = INORDEN, P= POSTORDEN,R=PREORDEN, DEPUES DE INGRESAR EL ARBOL";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.lborden);
+            this.Controls.Add(this.lbgrado);
+            this.Controls.Add(this.lbaltura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.ptb);
             this.Name = "Form1";
@@ -76,6 +161,13 @@
         private System.Windows.Forms.TextBox textBox1;
         public System.Windows.Forms.PictureBox ptb;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbaltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lborden;
+        private System.Windows.Forms.Label lbgrado;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Practica15/Arboles/Form1.cs
+++ Practica15/Arboles/Form1.cs
@@ -14,37 +14,99 @@ namespace Arboles
     {
         ArbolDibuja ad;
         nodo A;
+        int grado = 0, orden = 0;
+        int altura1 =0,altura2=0,altura3=0,altura4=0;
+        int hoja1,hoja2,hoja3,hoja4,node,raiz;
+        int start = 0;
+        List<int> alturas = new List<int>();
+        List<int> altura;
+
+
         public Form1()
         {
             InitializeComponent();
             ad = new ArbolDibuja(ptb);
             nodo A = new nodo();
             A = null;
+            lborden.Text = "2";
+            alturas.Add(0);
+            alturas.Add(1);
+            alturas.Add(0);
+            alturas.Add(0);
+
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Enter)
             {
-                
-                ad.inserta_nodo(ad.Raiz, A, int.Parse(textBox1.Text), 0);
+                node = int.Parse(textBox1.Text);
+                ad.inserta_nodo(ad.Raiz, A, node, 0);             
                 textBox1.Text = "";
+                if (start > 0)
+                {
+                    if (node > hoja1 && node< hoja4) {
+                        hoja2 = node;
+                        alturas[1] = altura[3] + 1;
+                    }
+                    if (node < hoja1)
+                    {
+                        hoja1 = node;
+                        alturas[0] = alturas[0] + 1;
+
+                    }
+                    if (node > hoja4)
+                    {
+                        hoja4 = node;
+                        alturas[3] = alturas[3] + 1;
+                    }
+                    
+                    altura = alturas;
+                    altura.Sort();
+                    lbaltura.Text = altura[3]+"";
+                   
+                    
+                    start++;
+                    if (start == 2 || start==3) {
+                        grado++;
+                    }
+                }
+                else {
+                    raiz = node;
+                    hoja1 = node;
+                    hoja2 = node;
+                    hoja3 = node;
+                    hoja4 = node;
+                    start ++;
+                }
+                lbgrado.Text = grado + "";
+
+
+
+
             }
             else if (e.KeyChar == (char)Keys.R)
             {
                 ad.preorden(ad.Raiz);
 
             }
-            else if (e.KeyChar == (char)Keys.P) {
+            else if (e.KeyChar == (char)Keys.P)
+            {
                 ad.posorden(ad.Raiz);
-       
+
             }
             else if (e.KeyChar == (char)Keys.I)
             {
                 ad.inorden(ad.Raiz);
-             
+
             }
-           
+            
+        
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
