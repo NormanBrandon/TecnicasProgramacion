e3a47c95c55110d97568af69a2043e19d98a3187
NormanBrandon <normanbrandons11@gmail.com>
2018-10-22 20:08:13 -0500

    Arboles

---
+++ Practica15/Arboles/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Arboles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ptb = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptb
+            // 
+            this.ptb.Location = new System.Drawing.Point(29, 60);
+            this.ptb.Name = "ptb";
+            this.ptb.Size = new System.Drawing.Size(742, 384);
+            this.ptb.TabIndex = 0;
+            this.ptb.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ptb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.PictureBox ptb;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Practica15/Arboles/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arboles
+{
+    public partial class Form1 : Form
+    {
+        ArbolDibuja ad;
+        nodo A;
+        public Form1()
+        {
+            InitializeComponent();
+            ad = new ArbolDibuja(ptb);
+            nodo A = new nodo();
+            A = null;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                
+                ad.inserta_nodo(ad.Raiz, A, int.Parse(textBox1.Text), 0);
+                textBox1.Text = "";
+            }
+            else if (e.KeyChar == (char)Keys.R)
+            {
+                ad.preorden(ad.Raiz);
+
+            }
+            else if (e.KeyChar == (char)Keys.P) {
+                ad.posorden(ad.Raiz);
+       
+            }
+            else if (e.KeyChar == (char)Keys.I)
+            {
+                ad.inorden(ad.Raiz);
+             
+            }
+           
+        }
+    }
+}
