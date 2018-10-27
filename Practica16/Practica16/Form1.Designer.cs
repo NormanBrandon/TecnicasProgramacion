695318147ae9275ba41f90b40a1dc99950b37227
Eduardo159 <eduardomarquez.98@hotmail.com>
2018-10-26 20:49:00 -0500

    Busqueda

--- Practica16/Practica16/Form1.Designer.cs
+++ Practica16/Practica16/Form1.Designer.cs
@@ -30,10 +30,15 @@
         {
             this.txtEntrada = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lbNum = new System.Windows.Forms.Label();
-            this.lbOrdenado = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.lbOrdenado = new System.Windows.Forms.Label();
+            this.lbNum = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar1 = new System.Windows.Forms.Button();
+            this.btnBuscar2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,6 +54,9 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtbBuscar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.lbOrdenado);
             this.groupBox1.Controls.Add(this.lbNum);
@@ -60,22 +68,15 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
-            // label1
+            // button1
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(97, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(318, 44);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Ingresa un chigo de numeros, uno por uno, dando enter por cada uno";
-            // 
-            // lbNum
-            // 
-            this.lbNum.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.lbNum.Location = new System.Drawing.Point(86, 134);
-            this.lbNum.Name = "lbNum";
-            this.lbNum.Size = new System.Drawing.Size(329, 110);
-            this.lbNum.TabIndex = 2;
+            this.button1.Location = new System.Drawing.Point(46, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ordenar en forma ascendente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lbOrdenado
             // 
@@ -85,21 +86,76 @@
             this.lbOrdenado.Size = new System.Drawing.Size(329, 98);
             this.lbOrdenado.TabIndex = 2;
             // 
-            // button1
+            // lbNum
             // 
-            this.button1.Location = new System.Drawing.Point(217, 262);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Ordenar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.lbNum.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lbNum.Location = new System.Drawing.Point(86, 134);
+            this.lbNum.Name = "lbNum";
+            this.lbNum.Size = new System.Drawing.Size(329, 110);
+            this.lbNum.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresa diversos números, uno por uno,            dando enter por cada uno.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ordenar en forma descendiente";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Elemento a buscar";
+            // 
+            // txtbBuscar
+            // 
+            this.txtbBuscar.Location = new System.Drawing.Point(239, 420);
+            this.txtbBuscar.Name = "txtbBuscar";
+            this.txtbBuscar.Size = new System.Drawing.Size(100, 20);
+            this.txtbBuscar.TabIndex = 6;
+            // 
+            // btnBuscar1
+            // 
+            this.btnBuscar1.Location = new System.Drawing.Point(130, 487);
+            this.btnBuscar1.Name = "btnBuscar1";
+            this.btnBuscar1.Size = new System.Drawing.Size(120, 23);
+            this.btnBuscar1.TabIndex = 2;
+            this.btnBuscar1.Text = "Buscar eficientemente";
+            this.btnBuscar1.UseVisualStyleBackColor = true;
+            this.btnBuscar1.Click += new System.EventHandler(this.btnBuscar1_Click);
+            // 
+            // btnBuscar2
+            // 
+            this.btnBuscar2.Location = new System.Drawing.Point(337, 487);
+            this.btnBuscar2.Name = "btnBuscar2";
+            this.btnBuscar2.Size = new System.Drawing.Size(131, 23);
+            this.btnBuscar2.TabIndex = 3;
+            this.btnBuscar2.Text = "Buscar ineficientemente";
+            this.btnBuscar2.UseVisualStyleBackColor = true;
+            this.btnBuscar2.Click += new System.EventHandler(this.btnBuscar2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 484);
+            this.ClientSize = new System.Drawing.Size(548, 522);
+            this.Controls.Add(this.btnBuscar2);
+            this.Controls.Add(this.btnBuscar1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -117,6 +173,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lbOrdenado;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtbBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBuscar1;
+        private System.Windows.Forms.Button btnBuscar2;
     }
 }
 
--- Practica16/Practica16/Form1.cs
+++ Practica16/Practica16/Form1.cs
@@ -86,5 +86,130 @@ namespace Practica16
                 quicksort(vector, i, ultimo);
             }
         }
+
+        public void quicksort2 (int[] vector, int primero, int ultimo)
+        {
+            
+            int i, j, central;
+            double pivote;
+            int aux;
+            central = (primero + ultimo) / 2;
+            pivote = vector[central];
+            i = primero;
+            j = ultimo;
+            do
+            {
+                while (vector[i] > pivote) i++;
+                while (vector[j] < pivote) j--;
+                if (i <= j)
+                {
+                    aux = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = aux;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (primero < j)
+            {
+                quicksort2(vector, primero, j);
+            }
+            if (i < ultimo)
+            {
+                quicksort2(vector, i, ultimo);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            V = new int[Vector.Count];
+            for (int i = 0; i < Vector.Count; i++)
+            {
+                V[i] = Vector[i];
+            }
+            quicksort2(V, 0, V.Length - 1);
+            for (int i = 0; i < V.Length; i++)
+            {
+                lbOrdenado.Text = lbOrdenado.Text + V[i] + " ";
+            }
+            
+        }
+
+        static int binario(int[] V, int X)
+        {
+            int izq = 0;
+            int der = V.Length - 1;
+            int centro = (izq + der) / 2;
+            bool encontrado = false;
+
+          
+
+            while (izq < der && !encontrado)
+            {
+                if (V[centro] == X)
+                {
+                    encontrado = true;
+                }
+               
+                else
+                {
+                    if (X > V[centro])
+                    {
+                        izq = centro + 1;
+                    }
+                    else
+                    {
+                        der = centro - 1;
+                    }
+                }
+                centro = (izq + der) / 2;
+                
+                if (V[izq] == X)
+                {
+                    encontrado = true;
+                }
+                
+            }
+
+            if (encontrado)
+            {
+                return centro;
+            }
+            
+            else
+            {
+                return -1;
+            }
+            
+        }
+
+        private void btnBuscar1_Click(object sender, EventArgs e)
+        {
+            int valor = int.Parse(txtbBuscar.Text);
+            int indice = binario(V, valor);
+
+            if (indice != -1)
+            {
+                MessageBox.Show("Valor encontrado en la posición: " + indice);
+            }
+
+            else
+            {
+                MessageBox.Show("Valor no encontrado");
+            }
+
+
+        }
+
+        private void btnBuscar2_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(txtbBuscar.Text) == )
+            {
+                MessageBox.Show("Valor encontrado en")
+            }
+        }
     }
 }
