84114991d1cc8caf183f1ffbdab687d6968a85f0
Eduardo159 <eduardomarquez.98@hotmail.com>
2018-11-08 20:30:46 -0600

    Practica 18

---
+++ Practica18/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FormularioArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbNotas = new System.Windows.Forms.RichTextBox();
+            this.txtbEntero = new System.Windows.Forms.TextBox();
+            this.txtbDouble = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProv = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // rtbNotas
+            // 
+            this.rtbNotas.Location = new System.Drawing.Point(12, 115);
+            this.rtbNotas.Name = "rtbNotas";
+            this.rtbNotas.Size = new System.Drawing.Size(564, 163);
+            this.rtbNotas.TabIndex = 1;
+            this.rtbNotas.Text = "";
+            // 
+            // txtbEntero
+            // 
+            this.txtbEntero.Location = new System.Drawing.Point(154, 304);
+            this.txtbEntero.Name = "txtbEntero";
+            this.txtbEntero.Size = new System.Drawing.Size(100, 20);
+            this.txtbEntero.TabIndex = 2;
+            // 
+            // txtbDouble
+            // 
+            this.txtbDouble.Location = new System.Drawing.Point(392, 304);
+            this.txtbDouble.Name = "txtbDouble";
+            this.txtbDouble.Size = new System.Drawing.Size(100, 20);
+            this.txtbDouble.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numeros enteros";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numeros doubles";
+            // 
+            // errorProv
+            // 
+            this.errorProv.ContainerControl = this;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(189, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 43);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mi primer Word";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Texto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(588, 347);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbDouble);
+            this.Controls.Add(this.txtbEntero);
+            this.Controls.Add(this.rtbNotas);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Mi primer Word";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtbNotas;
+        private System.Windows.Forms.TextBox txtbEntero;
+        private System.Windows.Forms.TextBox txtbDouble;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Practica18/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FormularioArchivos
+{
+    public partial class Form1 : Form
+    {
+        FileStream fs;
+        StreamReader sr;
+        BinaryReader br;
+        BinaryWriter bw;
+        FileInfo fi;
+        List<string> texto;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Texto|*.txt|Binarios|*.bin";//Restringes el tipo de archivo que puedes abrir
+
+            if( ofd.ShowDialog() == DialogResult.OK    )
+            {
+                if (ofd.Filter.Contains(".bin"))
+                {
+                    try
+                    {
+                        fi = new FileInfo(ofd.FileName);
+                        br = new BinaryReader(fi.OpenRead());
+                        rtbNotas.AppendText(br.ReadString());
+                        rtbNotas.AppendText(br.ReadInt32().ToString());
+                        rtbNotas.AppendText(br.ReadDouble().ToString()); 
+                    }
+                    catch (IOException error)
+                    {
+                        MessageBox.Show("Error: " + error.Message);
+                    }
+                    
+                    finally
+                    {
+                        br.Close();
+                    }
+                }
+                else
+                {
+                    texto = new List<string>();
+                    try
+                    {
+                        fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                        sr = new StreamReader(fs);
+                        string aux = sr.ReadLine();
+                        while (aux != null)
+                        {
+                            texto.Add(aux);
+                            aux = sr.ReadLine();
+                        }
+
+                    }
+                    catch (IOException error)
+                    {
+                        MessageBox.Show("Error :" + error.Message);
+                    }
+                    
+                    finally
+                    {
+                        fs.Close();
+                        sr.Close();
+                    }
+                    for (int i = 0; i < texto.Count; i++)
+                    {
+                        rtbNotas.AppendText(texto[i]);
+                    }
+                }
+
+            }
+
+
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Texto|*.txt|Binarios|*.bin";
+
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    fi = new FileInfo(sfd.FileName);
+                    bw = new BinaryWriter(fi.OpenWrite());
+
+                    string texto = rtbNotas.Text;
+                    int entero = int.Parse(txtbEntero.Text);
+                    double doble = double.Parse(txtbDouble.Text);
+
+                    bw.Write(texto);
+                    bw.Write(entero);
+                    bw.Write(doble);
+
+                    errorProv.Clear();
+                }
+                catch (IOException error)
+                {
+                    MessageBox.Show("Error" + error.Message);
+                }
+                catch (ApplicationException error)
+                {
+                    errorProv.SetError(txtbEntero, error.Message);
+                }
+                finally
+                {
+                    bw.Close();
+                }
+            }
+
+
+
+        }
+    }
+}
